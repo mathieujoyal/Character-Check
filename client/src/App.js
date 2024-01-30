7c08def6e55b696e24df94e0997abcd40f34ea15
Mathieu Joyal <122555134+mathieujoyal@users.noreply.github.com>
2024-01-30 15:51:47 -0500

    Revert "Calculator styling"

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,12 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Globalstyles from "./Globalstyles"
-import Homepage from "./Homepage"
-import Header from "./Header"
-import Calculator from "./Calculator"
-import Encyclopedia from "./Encyclopedia"
-import Login from "./Login"
-import Signup from "./Signup"
-import SheetMaker from "./SheetMaker"
 
-const App = () => {
+function App() {
+
+
   
   return (
-    <BrowserRouter>
-      <Header/>
-      <Globalstyles />
-      <Routes>
-        <Route path="/" element={<Homepage /> }/>
-        <Route path="/calculator" element={<Calculator />}/>
-        <Route path="/encyclopedia" element={<Encyclopedia />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/signup" element={<Signup />}/>
-        <Route path="/sheetmaker" element={<SheetMaker />}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Hello</h1>
+    </div>
   );
 }
 
